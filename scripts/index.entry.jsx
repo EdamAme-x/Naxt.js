18289fe144255727d000ebf3f95d69675f5783c0
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-04 09:56:50 +0900

    Update index.entry.jsx

--- scripts/index.entry.jsx
+++ scripts/index.entry.jsx
@@ -65,7 +65,7 @@ export class Naxt {
     // 先に画像等のファイルを検索 無かったら SearchPath で検索 そしてRenderServerSideJSX
     this._honoApp.get("/*", (c) => {
       const currentPath = c._path;
-      console.log("ACCESS: " + currentPath)
+      console.log("ACCESS: " + c)
 
       let alive_check_token = ""; // ホットリロード
 
