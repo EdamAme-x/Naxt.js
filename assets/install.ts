b5edaecf1e91f19417ea23be6e1a9f3c9ef1a008
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-01 18:29:44 +0900

    install cmd

---
+++ assets/install.ts
@@ -0,0 +1,26 @@
+import { moveSync } from "https://deno.land/x/std/fs/mod.ts";
+
+try {
+  const { args } = Deno;
+  const appName = args[0] ? args[0] : "naxtjs-app";
+  const repoUrl = "https://github.com/EdamAme-x/Naxt.js";
+
+  const isWindows = Deno.build.os === "windows";
+  const gitCmd = isWindows ? "git" : "git";
+  const rmCmd = isWindows ? "del" : "rm";
+
+  const gitProcess = Deno.run({
+    cmd: [gitCmd, "clone", repoUrl, "_" + appName],
+    cwd: Deno.cwd(),
+  });
+
+  await gitProcess.status();
+
+  moveSync("_" + appName + "/demo", appName);
+
+  await Deno.remove("_" + appName, {
+    recursive: true,
+  });
+} catch (e) {
+  console.error(e);
+}
